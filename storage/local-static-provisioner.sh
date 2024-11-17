85a657bf3c15ca91a26a4c41e284c703f5b01f77
Anton Putra <me@antonputra.com>
2024-11-17 21:26:40 +0500

    fix folders

--- storage/local-static-provisioner.sh
+++ storage/local-static-provisioner.sh
@@ -2,11 +2,8 @@
 
 ## Mount Disks
 
-sudo mkdir /mnt/disks/sdb
+sudo mkdir -p /mnt/disks/sdb
 sudo mkfs.xfs /dev/sdb
 sudo mount -o defaults /dev/sdb /mnt/disks/sdb
 sudo lsblk --fs
-echo "/dev/disk/by-uuid/c3495fc6-2993-432c-86c1-363658d1878d /mnt/disks/sdb xfs defaults 0 1" | sudo tee -a /etc/fstab
-
-
-
+echo "/dev/disk/by-uuid/dae66b8e-9923-4175-b97e-2061237957b7 /mnt/disks/sdb xfs defaults 0 1" | sudo tee -a /etc/fstab
