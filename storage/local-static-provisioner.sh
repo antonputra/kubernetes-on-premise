97feb200e13e6873ee9620cb1052160a230d267f
Anton Putra <me@antonputra.com>
2026-02-11 13:42:52 +0500

    update local-static-provisioner

--- storage/local-static-provisioner.sh
+++ storage/local-static-provisioner.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-## Mount Disks
+# Prepare local disks
 
-sudo mkdir -p /mnt/disks/sdb
-sudo mkfs.xfs /dev/sdb
-sudo mount -o defaults /dev/sdb /mnt/disks/sdb
-sudo lsblk --fs
-echo "/dev/disk/by-uuid/dae66b8e-9923-4175-b97e-2061237957b7 /mnt/disks/sdb xfs defaults 0 1" | sudo tee -a /etc/fstab
+export DISK_SIZE="50G"
+export DISK_NAME=$(sudo lsblk --json | jq -r --arg size "$DISK_SIZE" '.blockdevices[] | select(.type == "disk" and .size == $size and .ro == false) | .name')
+echo $DISK_NAME
+
+sudo mkdir -p /mnt/disks/$DISK_NAME
+sudo mkfs.xfs /dev/$DISK_NAME
+sudo mount -o defaults /dev/$DISK_NAME /mnt/disks/$DISK_NAME
+
+export DISK_UUID=$(sudo lsblk --fs --json | jq -r --arg name "$DISK_NAME" '.blockdevices[] | select(.name == $name) | .uuid')
+echo "/dev/disk/by-uuid/$DISK_UUID /mnt/disks/$DISK_NAME xfs defaults 0 1" | sudo tee -a /etc/fstab
+cat /etc/fstab
